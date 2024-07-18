c2e915cf497ad9218c7f31963880394f6b9868e3
Lokesh Kumar <lokesh2.yss@gmail.com>
2024-07-18 12:30:24 +0530

    Initial commit

---
+++ src/main/java/com/codingshuttle/app/zomatoApp/ZomatoAppApplication.java
@@ -0,0 +1,13 @@
+package com.codingshuttle.app.zomatoApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZomatoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZomatoAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codingshuttle/app/zomatoApp/ZomatoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingshuttle.app.zomatoApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZomatoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
